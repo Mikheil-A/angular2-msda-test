61ec2b469c55398e03728a95f7de929c91bfdce0
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-12-13 17:26:39 +0400

    recently worked version (except tslint cant find angularmsdamodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 
-import {AngularMsdaModule} from 'angular-msda';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSliderModule} from '@angular/material/slider';
+
+import {AngularMsdaModule} from 'angular-msda';
+
 
 
 @NgModule({
